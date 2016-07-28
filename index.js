444e0c12e1ec5f058365d3e57b741bd39e03ef91
ross <rossc@globalia.ca>
2016-07-28 15:59:32 -0400

    having fun

--- index.js
+++ index.js
@@ -3,33 +3,45 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var colors = require('colors');
 var five = require("johnny-five");
-var board = new five.Board();
+//var board = new five.Board();
+
+
+
 
 app.get('/', function(req, res){
-  res.sendfile('index.html');
+    res.sendfile('index.html');
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
+    var socketId = socket.id;
+ var clientIp = socket.request.connection.remoteAddress;
+
+ console.log("New connection: "+ clientIp);
+ io.emit('code', 'New connection: '+ clientIp);
 });
 
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+    console.log('listening on *:3000');
 });
 
 
 io.on('connection', function(socket){
-  socket.on('code', function(msg){
-    console.log('message: ' + msg);
-    if (msg == 1) {
-        console.log('OMG Rainbows!'.green);
-    }
-    else {
-        console.log('Unknow code'.inverse);
-    }
-  });
+    socket.on('code', function(msg){
+        console.log('message: ' + msg);
+        if (msg == 1) {
+            console.log('OMG Rainbows!'.green);
+            io.emit('code', "Start application");
+        }
+        else {
+            console.log('Unknow code'.inverse);
+            io.emit('code', 'Unknow code');
+        }
+    });
 });
 
+setInterval(function() {
+    io.emit('code', 'Ping');
+}, 2000);
 
 //
 // board.on("ready", function() {
