f7584884b2116a464c2a25fc90f9c333768adc4a
ross <rossc@globalia.ca>
2016-07-28 14:45:48 -0400

    j5

--- index.js
+++ index.js
@@ -31,13 +31,13 @@ io.on('connection', function(socket){
 });
 
 
-
-board.on("ready", function() {
-
-  // Create a standard `led` component instance
-  var led = new five.Led(13);
-
-  // "blink" the led in 500ms
-  // on-off phase periods
-  led.blink(500);
-});
+//
+// board.on("ready", function() {
+//
+//   // Create a standard `led` component instance
+//   var led = new five.Led(13);
+//
+//   // "blink" the led in 500ms
+//   // on-off phase periods
+//   led.blink(500);
+// });
